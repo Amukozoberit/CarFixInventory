a424fede44805ddd49141453d1a9c9a58c9194a7
Amukozoberit <mwasheb@gmail.com>
2022-03-28 18:57:38 +0300

    rename

--- carFixInventoryApp/urls.py
+++ carFixInventoryApp/urls.py
@@ -5,8 +5,8 @@ from carFixInventoryApp import views
 urlpatterns = [
     path('inventoryView/', views.InventorySnippetList.as_view()),
     path('inventoryView/<int:pk>', views.InventorySnippetDetail.as_view()),
-    path('mechanicsview/', views.MechanicViewSet.as_view({'get': 'list'})),
-    path('mechanicpost/', views.MechanicViewSet.as_view({'post': 'create'})),
+    path('mechanicsandservicesview/', views.MechanicViewSet.as_view({'get': 'list'})),
+    path('mechanicandservicespost/', views.MechanicViewSet.as_view({'post': 'create'})),
     path('serviceview/', views.ServiceViewSet.as_view({'get': 'list'})),
     path('servicepost/', views.ServiceViewSet.as_view({'post': 'create'})),
 
